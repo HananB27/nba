9c074308cd3ba301b3e85414c5088ff3ee7e5cd8
HananB27 <hananbajramovic075@gmail.com>
2023-04-11 15:31:37 +0200

    add paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GamesComponent } from './games/games.component';
+import { TeamsComponent } from './teams/teams.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:GamesComponent},
+  {path:'teams', component:TeamsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
