935433d85fa671625968d154b5124db74b3ddfe4
HananB27 <hananbajramovic075@gmail.com>
2023-04-11 15:52:09 +0200

    make api work for teams

--- src/app/Service/nba-api.service.ts
+++ src/app/Service/nba-api.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NbaApiService {
 
-  constructor() { }
+  private baseUrl = 'https://free-nba.p.rapidapi.com';
+
+  constructor(private http: HttpClient) { }
+
+  getTeams(): Observable<any> {
+    const options = {
+      params: { page: '0' },
+      headers: {
+        'X-RapidAPI-Key': '70911187c4mshdb3dfd60b77a1b0p18b79fjsne934449af779',
+        'X-RapidAPI-Host': 'free-nba.p.rapidapi.com'
+      }
+    };
+    return this.http.get(`${this.baseUrl}/teams`, options);
+  }
+
+  getGames(): Observable<any> {
+    const options = {
+      params: { page: '0', per_page: '25' },
+      headers: {
+        'X-RapidAPI-Key': '70911187c4mshdb3dfd60b77a1b0p18b79fjsne934449af779',
+        'X-RapidAPI-Host': 'free-nba.p.rapidapi.com'
+      }
+    };
+    return this.http.get(`${this.baseUrl}/games`, options);
+  }
 }
--- src/app/teams/teams.component.ts
+++ src/app/teams/teams.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NbaApiService } from '../Service/nba-api.service';
 
 @Component({
   selector: 'app-teams',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./teams.component.scss']
 })
 export class TeamsComponent {
+  teams: any[] = [];
 
+  constructor(private nbapiService: NbaApiService) { }
+
+  ngOnInit(): void {
+    this.nbapiService.getTeams().subscribe(
+      (response) => {
+        this.teams = response.data;
+        console.log(this.teams);
+      }
+    );
+  }
 }
