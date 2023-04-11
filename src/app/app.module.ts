41d635b15d5f8230e44fe1322859c9a71544f6fc
HananB27 <hananbajramovic075@gmail.com>
2023-04-11 15:33:12 +0200

    add header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { TeamsComponent } from './teams/teams.component';
 import { GamesComponent } from './games/games.component';
 import {HttpClientModule} from '@angular/common/http'
 import { NbaApiService } from './Service/nba-api.service';
+import { HeaderComponent } from './Shared/header/header.component';
+import { FooterComponent } from './Shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TeamsComponent,
-    GamesComponent
+    GamesComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
